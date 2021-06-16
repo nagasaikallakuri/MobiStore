81f50506d422233f871b13fe860cd1d30298ff14
NagaSai Kallakuri <nagasaikallakuri@NagaSais-MacBook-Pro.local>
2021-06-15 23:43:00 -0700

    First Commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {ProductProvider} from './context';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProductProvider>
+    <Router><App /></Router>   
+    </ProductProvider> 
   </React.StrictMode>,
   document.getElementById('root')
 );
